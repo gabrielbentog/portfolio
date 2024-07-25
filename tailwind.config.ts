f00125df5f06415b4037c92ad1ba51848b06ca00
gabrielbentog <bentogdev@gmail.com>
2024-07-25 16:12:18 -0300

    refactor: finish configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,35 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        'ligh-gray-gradient': "linear-gradient(to right, #f5f5f5, #e0e0e0)",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out"
+      },
+      transform: {
+        'rotate-y-180': 'rotateY(180deg)',
+      },
+      perspective: {
+        '1000': '1000px',
+      },
+      backfaceVisibility: {
+        'hidden': 'hidden',
+      },
+      colors: {
+        // Adicione ou customize cores para o tema escuro
+        'dark-bg': '#1a202c',
+        'dark-text': '#f7fafc',
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
